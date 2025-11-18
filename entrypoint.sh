ca5789d76e8a7db084b78ebf59af984f6a7eefe5
lazy_sl0th <anton.vlrch.ch@gmail.com>
2025-11-18 22:40:47 +0400

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,12 @@ OUTPUT="/etc/odoo/odoo.conf"
 
 eval "echo \"$(sed 's/\"/\\"/g' $TEMPLATE)\"" > $OUTPUT
 
-echo ">> Checking if database needs initialization..."
+echo ">> Waiting for PostgreSQL..."
 
-CHECK_DB=$(psql "host=$DATABASE_HOST port=$DATABASE_PORT user=$DATABASE_USER password=$DATABASE_PASSWORD dbname=$DATABASE_NAME" -tAc "SELECT COUNT(*) FROM pg_tables WHERE tablename = 'ir_module_module';" || echo "0")
+until pg_isready -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$DATABASE_USER"; do
+  echo "Database not ready yet..."
+  sleep 1
+done
 
-if [ "$CHECK_DB" = "0" ]; then
-  echo ">> Database is empty. Initializing base module..."
-  exec odoo --config="$OUTPUT" -i base --load-language=en_US
-else
-  echo ">> Database already initialized. Running Odoo normally..."
-  exec odoo --config="$OUTPUT"
-fi
+echo ">> Starting Odoo..."
+exec odoo --config="$OUTPUT"
