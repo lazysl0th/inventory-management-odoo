7b4a7898780bd92b86233b2bc2934dfa4bac5ce8
lazy_sl0th <anton.vlrch.ch@gmail.com>
2025-11-18 20:35:23 +0400

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,17 @@
 set -e
 
 echo ">> Generating Odoo configuration from template..."
-envsubst < /etc/odoo/odoo.conf.tpl > /etc/odoo/odoo.conf
+
+CONFIG_FILE=/etc/odoo/odoo.conf
+
+# Replace environment variables manually using sed
+sed \
+  -e "s|\${DATABASE_HOST}|${DATABASE_HOST}|g" \
+  -e "s|\${DATABASE_PORT}|${DATABASE_PORT}|g" \
+  -e "s|\${DATABASE_USER}|${DATABASE_USER}|g" \
+  -e "s|\${DATABASE_PASSWORD}|${DATABASE_PASSWORD}|g" \
+  -e "s|\${DATABASE_NAME}|${DATABASE_NAME}|g" \
+  /etc/odoo/odoo.conf.tpl > $CONFIG_FILE
 
 echo ">> Starting Odoo..."
-exec odoo --config=/etc/odoo/odoo.conf
-
+exec odoo --config=$CONFIG_FILE
